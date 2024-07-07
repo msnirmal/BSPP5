a41ad060b3f611237c1037226741c8837c2a8bf9
Nirmal Kumar Subramani <kumar.n.subramani@gmail.com>
2024-07-07 15:48:57 +0000

    Add checkout views and templates

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +63,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'bookshop.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -76,9 +81,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', 
                 'django.contrib.auth.context_processors.auth', 
                 'django.contrib.messages.context_processors.messages',
-                'cart.contexts.cart_summary'
-                
+                'django.template.context_processors.media',
+                'cart.contexts.cart_summary',                
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
