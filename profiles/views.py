fa43fd90242541b4ea6c5c494703a060bc4bf1c0
Nirmal Kumar Subramani <kumar.n.subramani@gmail.com>
2024-07-17 17:36:26 +0000

    Add profile form, views and update template

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -20,7 +21,8 @@ def profile(request):
     template = 'profiles/profile.html'
     context = {
         'form': form,
-        'orders': orders,        
+        'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
